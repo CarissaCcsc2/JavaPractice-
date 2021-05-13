475b52c20bea44e7c3f09f8819ea0ffdc2c61fdb
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-05-13 17:56:53 -0400

    example8

--- example8.java
+++ example8.java
@@ -1,6 +1,12 @@
 public class example8 {
     public static void main(String[] args) {
+        //Char variables behave like integers
+        char ch1;
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
 
+        ch1++;
+        System.out.println("Ch1 is now " + ch1);
 
     }
 }
