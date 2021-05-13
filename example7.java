e5de8db3ec27ad3d0898c0d1a510c8e273ada588
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-05-13 15:24:27 -0400

    adding new

---
+++ example7.java
@@ -0,0 +1,15 @@
+public class example7 {
+    public static void main(String[] args) {
+        //demonstrate char data type
+        char ch1, ch2;
+
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+
+
+
+    }
+}
