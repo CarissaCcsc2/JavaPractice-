03da288aac1bf66f1937cd90837207e98dcca2de
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-05-13 15:24:24 -0400

    adding new

---
+++ example6.java
@@ -0,0 +1,14 @@
+public class example6 {
+    public static void main(String[] args) {
+        //Compute the area of a circle 
+        double pi, r, a;
+
+        r = 10.8; //radius of circle
+        pi = 3.1416; //pi approximately 
+        a = pi * r * r; //compute area 
+
+        System.out.println("Area of a circle is " + a);
+
+
+    }
+}
