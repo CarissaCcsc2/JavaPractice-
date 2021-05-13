434299aa143153ec1065a1f72a62c017a502d58b
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-05-13 15:24:17 -0400

    adding new

---
+++ example4.java
@@ -0,0 +1,18 @@
+public class example4 {
+    public static void main(String[] args) {
+        int x, y;
+        y = 20;
+        
+        for(x = 0; x < 10; x++){
+            System.out.println("This is x:" + x);
+            System.out.println("This is y:" + y);
+            y = y-2;
+
+            
+
+
+        }
+
+
+    }
+}
