a4238d60820df56f5d688458e1be3307284d4042
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-05-19 22:25:44 -0400

    Dynamic initialization of variables

---
+++ dynamicInitialization.java
@@ -0,0 +1,16 @@
+/**
+ * dynamicInitialization
+ */
+public class dynamicInitialization {
+    //Calculating the hypotenuse 
+    public static void main(String[] args) {
+        //Demonstrate dynamic initialization 
+        int a = 3, b = 4;
+
+        //c is dynamically initialized 
+        double c =  Math.sqrt(a*a+b*a);
+
+        System.out.println("The Hypotenuse is " + c);
+    }
+    
+}
